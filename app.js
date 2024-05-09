1f8ec89c31195906a340e5bdc600da19eeaef454
Ashar Khan <asharullah.khan2468@gmail.com>
2024-05-10 00:11:05 +0500

    Chap 4 file js

---
+++ app.js
@@ -0,0 +1,30 @@
+//Question no 1
+
+var name age email;
+
+//Question no 2
+
+//legal variables
+
+var userName="Ashar";
+var fatherName='Irfan';
+var grade=9;
+var rollNo=1234;
+var field="Software Developer";
+
+//illegal variables
+
+var name="khan";
+var %alert="khan";
+var *grade="khan";
+var Fathername="khan";
+var +field="khan";
+
+
+//Question no 3
+
+document.write(`<h1>Rules for naming JS variables</h1>
+<p>Variable names can only contain numbers , $ and_ for example my_1stvariable</p>
+<p>Variables must begin with a letter , $ and _ for example $name , _name , or name</p>
+<p> Variable names are case sensitive</p>
+<p> Variable names should not be JS Keywords</p>`);
